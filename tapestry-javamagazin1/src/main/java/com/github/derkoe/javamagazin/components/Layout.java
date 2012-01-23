7f3671f307d5908f9564b3289f8229ce08ac9cfd
derkoe <christian.koeberl@gmail.com>
2012-01-23 21:01:15 +0100

    initial version of demo for Tapestry article

---
+++ tapestry-javamagazin1/src/main/java/com/github/derkoe/javamagazin/components/Layout.java
@@ -0,0 +1,51 @@
+package com.github.derkoe.javamagazin.components;
+
+import static java.util.Arrays.asList;
+
+import java.util.Collection;
+
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+
+/**
+ * Layout component for pages of application tapestry-javamagazin1.
+ */
+@Import(stack = "bootstrap")
+public class Layout
+{
+    /**
+     * The page title, for the <title> element and the <h1> element.
+     */
+    @Property
+    @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
+    private String title;
+
+    @Property
+    private String pageName;
+
+    @Inject
+    private ComponentResources resources;
+
+    @Property
+    @Inject
+    @Symbol(SymbolConstants.APPLICATION_VERSION)
+    private String appVersion;
+
+    public String getClassForPageName()
+    {
+        return resources.getPageName().equals(pageName)
+                ? "active"
+                : null;
+    }
+
+    public Collection<String> getPageNames()
+    {
+        return asList("Contact", "About");
+    }
+}
---
+++ tapestry-javamagazin1/src/main/java/com/github/derkoe/javamagazin/pages/About.java
@@ -0,0 +1,6 @@
+package com.github.derkoe.javamagazin.pages;
+
+public class About
+{
+
+}
