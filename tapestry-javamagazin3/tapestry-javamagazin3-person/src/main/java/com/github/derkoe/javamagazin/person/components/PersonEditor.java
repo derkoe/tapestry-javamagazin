683f7dfd8b83d8f1112c066629e8ec3894553f20
derkoe <christian.koeberl@gmail.com>
2012-04-26 22:03:18 +0200

    changed to Tapestry 5.3.3

--- tapestry-javamagazin3/tapestry-javamagazin3-person/src/main/java/com/github/derkoe/javamagazin/person/components/PersonEditor.java
+++ tapestry-javamagazin3/tapestry-javamagazin3-person/src/main/java/com/github/derkoe/javamagazin/person/components/PersonEditor.java
@@ -1,13 +1,16 @@
 package com.github.derkoe.javamagazin.person.components;
 
 import org.apache.tapestry5.annotations.Parameter;
-import org.apache.tapestry5.annotations.Property;
 
 import com.github.derkoe.javamagazin.person.services.Person;
 
 public class PersonEditor
 {
     @Parameter(required = true)
-    @Property
     private Person person;
+
+    public Person getPerson()
+    {
+        return person;
+    }
 }
